4ce57ba4e972daaf3dd9db313a274c514aa78121
Jamie Brown <jmbrowncow@gmail.com>
2018-07-09 19:40:02 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,10 @@
 class Song
-  attr_accessor :artist
+  attr_accessor :artist, :name
   @@all = []
+
+  def initialize(name)
+      self.name = name
+  end
 
   def artist_name
     self.artist.name
