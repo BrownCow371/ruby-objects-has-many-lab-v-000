608080089f6b5c918bf8a72a6504a33686743a4b
Jamie Brown <jmbrowncow@gmail.com>
2018-07-09 19:59:26 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,8 +1,13 @@
 class Post
   attr_accessor :author, :title
-
+  @@all=[]
+  
   def initialize(title)
     self.title = title
+    self.class.all<<self
   end
 
+  def self.all
+    @@all
+  end
 end
